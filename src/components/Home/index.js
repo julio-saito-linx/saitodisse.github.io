0e475cda982fb6c0fcd35ed3a44480a09c629198
Julio Makdisse Saito <saitodisse@gmail.com>
2016-12-27 19:03:29 -0200

    feat(Eject): eject from deafult create-react-app

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,65 +4,194 @@ import './styles.css'
 export default () => (
   <div id='home-container'>
 
-    <header>
-      <div id='pic-and-title'>
-        <img
-          className='profile-photo'
-          src='/images/IMG_20161029_030344_processed.jpg'
-          alt='profile photo'
-        />
-        <h1>
-          Julio Makdisse Saito
-        </h1>
+    <section>
+      <div className='top-info'>
+        <div id='pic-and-title'>
+          <img
+            className='profile-photo'
+            src='/images/IMG_20161029_030344_processed.jpg'
+            alt='profile photo'
+          />
+        </div>
+        <div className='contact-info'>
+          <div className='list-item'>
+            Julio Makdisse Saito
+          </div>
+          <div className='list-item'>
+            <a href='https://github.com/saitodisse?tab=repositories'>github</a>
+          </div>
+          <div className='list-item'>
+            <a href='mailto:saitodisse@gmail.com'>saitodisse@gmail.com</a>
+          </div>
+          <div className='list-item'>
+            <a href='tel:(+55) 11 965-697-834'>(+55) 11 965-697-834</a>
+          </div>
+          <div className='list-item'>
+            skype_id: saitodisse
+          </div>
+          <div className='list-item'>
+            birth year: 1981
+          </div>
+          <div className='list-item'>
+            nationality: Brazil
+          </div>
+        </div>
       </div>
-      <h2>Contact</h2>
-      <ul>
-        <li>
-          <a href='https://github.com/saitodisse?tab=repositories'>github</a>
-        </li>
-        <li>
-          <a href='mailto:saitodisse@gmail.com'>saitodisse@gmail.com</a>
-        </li>
-        <li>
-          <a href='tel:(+55) 11 965-697-834'>(+55) 11 965-697-834</a>
-        </li>
-        <li>
-          skype_id: saitodisse
-        </li>
-        <li>
-          from: São Bernardo do Campo - SP - Brazil
-        </li>
-      </ul>
-    </header>
+    </section>
 
     <section>
       <h2>Experience</h2>
+      <div className='group'>
+        <div className='title'>
+          Azuki
+        </div>
+        <div className='job'>
+          developer
+        </div>
+        <div className='date'>
+          Oct 2014 → Nov 2016
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            javascript
+          </div>
+          <div className='tag'>
+            node.js
+          </div>
+          <div className='tag'>
+            react
+          </div>
+          <div className='tag'>
+            react native
+          </div>
+          <div className='tag'>
+            cerebral
+          </div>
+          <div className='tag'>
+            mobx
+          </div>
+          <div className='tag'>
+            redux
+          </div>
+          <div className='tag'>
+            shell-script
+          </div>
+        </div>
+      </div>
 
       <div className='group'>
-        <div>
-          Date
+        <div className='title'>
+          Integgro <i className='title-small'>(as consultant)</i>
         </div>
-        <div>
-          Course
+        <div className='job'>
+          developer
         </div>
-        <div>
-          Description
+        <div className='date'>
+          Aug 2013 → Nov 2013
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            c#
+          </div>
+          <div className='tag'>
+            javascript
+          </div>
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          Grupo SHC <i className='title-small'>(Citroen and JAC Motors)</i>
+        </div>
+        <div className='job'>
+          developer
+        </div>
+        <div className='date'>
+          2006 → 2012
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            c#
+          </div>
+          <div className='tag'>
+            javascript
+          </div>
+          <div className='tag'>
+            asp.net-mvc
+          </div>
+          <div className='tag'>
+            knockout.js
+          </div>
+          <div className='tag'>
+            jquery
+          </div>
+          <div className='tag'>
+            kendoui
+          </div>
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          Everis, Consulting, IT, Outsourcing & Professional Services
+        </div>
+        <div className='job'>
+          developer
+        </div>
+        <div className='date'>
+          May 2005 → Feb 2006
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            javascript
+          </div>
+          <div className='tag'>
+            asp
+          </div>
+          <div className='tag'>
+            vb6
+          </div>
         </div>
       </div>
     </section>
 
     <section>
       <h2>Education</h2>
-
       <div className='group'>
-        <div>
-          Date
+        <div className='title'>
+          Engineering technologist Computer data processing
         </div>
-        <div>
-          Course
+        <div className='job'>
+          FATEC SP
         </div>
-        <div>
-          Description
+        <div className='date'>
+          2001 → 2007
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            c
+          </div>
+          <div className='tag'>
+            pascal
+          </div>
+          <div className='tag'>
+            java
+          </div>
+          <div className='tag'>
+            sql
+          </div>
+          <div className='tag'>
+            algorithm
+          </div>
+          <div className='tag'>
+            databases
+          </div>
+          <div className='tag'>
+            business
+          </div>
+          <div className='tag'>
+            project-management
+          </div>
         </div>
       </div>
     </section>
@@ -71,14 +200,142 @@ export default () => (
       <h2>Projects</h2>
 
       <div className='group'>
-        <div>
-          Date
+        <div className='title'>
+          pixijs-cerebral
         </div>
-        <div>
-          Course
+        <div className='date'>
+          Dec 2016 → Dec 2016
         </div>
-        <div>
-          Description
+        <div className='tags'>
+          <div className='tag'>
+            cerebral
+          </div>
+          <div className='tag'>
+            pixijs
+          </div>
+          <div className='tag'>
+            reactjs
+          </div>
+
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          reactNativeCerebral2
+        </div>
+        <div className='date'>
+          Dec 2016 → Dec 2016
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            react-native
+          </div>
+          <div className='tag'>
+            cerebral
+          </div>
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          markdown chat
+        </div>
+        <div className='date'>
+          Aug 2016 → Oct 2016
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            cerebral
+          </div>
+          <div className='tag'>
+            firebase
+          </div>
+          <div className='tag'>
+            elasticsearch
+          </div>
+          <div className='tag'>
+            notifications
+          </div>
+          <div className='tag'>
+            flexbox
+          </div>
+          <div className='tag'>
+            elasticsearch-2.0
+          </div>
+          <div className='tag'>
+            reactjs
+          </div>
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          twitter-stream-cli
+        </div>
+        <div className='date'>
+          Feb 2016 → Jul 2016
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            firebase
+          </div>
+          <div className='tag'>
+            twitter
+          </div>
+          <div className='tag'>
+            rxjs
+          </div>
+          <div className='tag'>
+            chalk
+          </div>
+          <div className='tag'>
+            gulp
+          </div>
+          <div className='tag'>
+            babeljs
+          </div>
+        </div>
+      </div>
+
+      <div className='group'>
+        <div className='title'>
+          azk
+        </div>
+        <div className='date'>
+          Sep 2013 → Feb 2016
+        </div>
+        <div className='tags'>
+          <div className='tag'>
+            javascript
+          </div>
+          <div className='tag'>
+            node.js
+          </div>
+          <div className='tag'>
+            shell
+          </div>
+          <div className='tag'>
+            docker
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <h2>External links</h2>
+      <div className='group'>
+        <div className='external-links'>
+          <div>
+            <a href='http://stackoverflow.com/cv/saitodisse' title='Curriculum vitae on stack overflow'>
+              cv
+            </a>
+          </div>
+          <div>
+            <a href='https://www.linkedin.com/in/julio-makdisse-saito-868707a' title='Curriculum vitae at linkedin'>
+              linkedin
+            </a>
+          </div>
         </div>
       </div>
     </section>
